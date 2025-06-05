e12da03b324275a36591d1a3a3213ebe65004735
c3r5b8 <olespid@gmail.com>
2025-06-05 19:34:39 +0300

    [feat]: add rpmfusion

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 dnf5 install -y\
 	tmux sway swaybg mako wl-clipboard grim slurp alacritty git fish\
 	firefox telegram dnf5-plugins
