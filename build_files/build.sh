f3d40ce07d55fd3752070d7522c44aafae178bdd
c3r5b8 <olespid@gmail.com>
2025-06-05 21:25:25 +0300

    [feat]: add sunshine and mask systemd-remount-fs.service

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,10 +8,12 @@ dnf5 install -y\
 	tmux sway swaybg mako wl-clipboard grim slurp alacritty git fish\
 	firefox telegram dnf5-plugins
 
-# dnf5 -y copr enable lizardbyte/stable
-# dnf5 -y install Sunshine
-# dnf5 -y copr disable lizardbyte/stable
+dnf5 -y copr enable lizardbyte/beta
+dnf5 -y install Sunshine
+dnf5 -y copr disable lizardbyte/beta
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+systemctl mask systemd-remount-fs.service
