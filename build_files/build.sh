de6dd112d8826efe9069149e68d4b83fd4037dc6
c3r5b8 <olespid@gmail.com>
2025-06-05 09:47:16 +0300

    [temp]: disable copr for now

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 dnf5 install -y\
 	tmux sway swaybg mako wl-clipboard grim slurp alacritty git fish
 
-dnf5 -y copr enable lizardbyte/stable
-dnf5 -y install Sunshine
-dnf5 -y copr disable lizardbyte/stable
+# dnf5 -y copr enable lizardbyte/stable
+# dnf5 -y install Sunshine
+# dnf5 -y copr disable lizardbyte/stable
 
 #### Example for enabling a System Unit File
 
