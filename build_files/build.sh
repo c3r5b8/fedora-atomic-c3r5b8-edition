0686613e49e134732a7f99c741740aff0ba7f88b
c3r5b8 <olespid@gmail.com>
2025-06-05 17:07:47 +0300

    [feat]: add firefox, telegram, sunshine

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 
 dnf5 install -y\
-	tmux sway swaybg mako wl-clipboard grim slurp alacritty git fish
+	tmux sway swaybg mako wl-clipboard grim slurp alacritty git fish\
+	firefox telegram dnf5-plugins
 
-# dnf5 -y copr enable lizardbyte/stable
-# dnf5 -y install Sunshine
-# dnf5 -y copr disable lizardbyte/stable
+dnf5 -y copr enable lizardbyte/stable
+dnf5 -y install Sunshine
+dnf5 -y copr disable lizardbyte/stable
 
 #### Example for enabling a System Unit File
 
